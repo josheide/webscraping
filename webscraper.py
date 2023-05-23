517eb54576321b776a661d2de7151dabb87472ad
Tamas Csillag <tamas.csillag@nyu.edu>
2023-05-23 15:44:27 +0200

    You can now scan the sites of your choice.
    Still very basic but this program can now take a user input and scrape the titles from that site. Still only goes for the h1 and h2 tags.

--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://index.hu/"
+print("Welcome to my Web Scraper!")
 
-result = requests.get(url)
+while True:
+    url = input("Please enter the website URL you want to scrape (or press Enter for default - index.hu): ")
 
-doc = BeautifulSoup(result.text, "html.parser")
+    if not url:
+        url = "https://index.hu/"
 
-titles = doc.find_all('h1') + doc.find_all('h2')
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-for title in titles:
-    print("- " + title.text.strip())
-    print()
+    titles = doc.find_all('h1') + doc.find_all('h2')
+
+    print("Scraped Titles:")
+    for item in titles:
+        title_text = item.text.strip()
+        print(" - " + item.text.strip())
+        
+        with open('scraped_titles.txt', 'a', encoding='utf-8') as file:
+            file.write(" - " + title_text + '\n')
+
+    user_input = input("Do you want to scrape another website? (yes/no): ")
+    if user_input.lower() != "yes":
+        break
+    
