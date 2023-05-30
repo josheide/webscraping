e663a11a09e0672c4bbf1914b01a65e20479e8ee
josheide <tamas.csillag@nyu.edu>
2023-05-30 12:27:05 +0200

    URLs are also scraped now!
    The code has been reorganised into a Webscraper class that has several methods. The welcome message moved into a separate .py file. URLs are now scraped along with the titles using the title and href HTML tags.

--- webscraper.py
+++ webscraper.py
@@ -1,28 +1,64 @@
 from bs4 import BeautifulSoup
+from welcome_message import print_welcome_message
 import requests
+from urllib.parse import urlparse
 
-print("Welcome to my Web Scraper!")
 
-while True:
-    url = input("Please enter the website URL you want to scrape (or press Enter for default - index.hu): ")
+class Webscraper:
+    def __init__(self):
+        self.scraped_data = []
 
-    if not url:
-        url = "https://index.hu/"
-
-    result = requests.get(url)
-    doc = BeautifulSoup(result.text, "html.parser")
-
-    titles = doc.find_all('h1') + doc.find_all('h2')
-
-    print("Scraped Titles:")
-    for item in titles:
-        title_text = item.text.strip()
-        print(" - " + item.text.strip())
-        
-        with open('scraped_titles.txt', 'a', encoding='utf-8') as file:
-            file.write(" - " + title_text + '\n')
-
-    user_input = input("Do you want to scrape another website? (yes/no): ")
-    if user_input.lower() != "yes":
-        break
+    def get_user_input(self):
+        while True:
+            url = input("Please enter the website URL you want to scrape (or press Enter for default - index.hu): ")
+            
+            if not url: 
+                return "https://index.hu/"
     
+            parsed_url = urlparse(url)
+            if parsed_url.scheme and parsed_url.netloc:
+                return url
+            else:
+                print("Invalid URL. Please enter a valid URL.")
+    
+    def scrape_website(self, url):
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text, "html.parser")
+        titles = doc.find_all('a')
+
+        print("Scraped Titles and URLs:")
+        for item in titles:
+            title_text = item.get('title')
+            url_text = item.get('href')
+
+            if title_text and url_text:
+                data_tuple = (title_text, url_text)
+                self.scraped_data.append(data_tuple)  # Append the tuple to the list
+
+                print("Title: " + title_text)
+                print("URL: " + url_text)
+                print(" ")
+                
+    def save_to_file(self):
+        with open('scraped_data.txt', 'a', encoding='utf-8') as file:
+            for data_tuple in self.scraped_data:
+                file.write("Title: " + data_tuple[0] + '\n')
+                file.write("URL: " + data_tuple[1] + '\n')
+                file.write('\n')
+                
+def main():
+    scraper = Webscraper()
+    print_welcome_message()
+        
+    while True:
+        url = scraper.get_user_input()
+        scraper.scrape_website(url)
+        scraper.save_to_file()
+    
+        user_input = input("Do you want to scrape another website? (yes/no): ")
+        
+        if user_input.lower() != "yes":
+            break
+
+if __name__ == '__main__':
+    main()
---
+++ welcome_message.py
@@ -0,0 +1,6 @@
+def print_welcome_message():
+    print("Welcome to my Web Scraper!")
+    print(" ")
+    print("This program scans a website's ""title"" and ""href"" tags.")
+    print("It saves what it finds to a .txt file on your PC")
+    print(" ")
